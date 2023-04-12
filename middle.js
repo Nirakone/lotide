4c7239c579245ffdfeb38f26971e5627770abbce
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-04-11 22:15:54 -0400

    commented removed. rewrite for other cases to work

--- middle.js
+++ middle.js
@@ -1,21 +1,11 @@
-const middle = (arrayA) => {
-  let middleItem = [];
+const middle = function(arrayA) {
   if (arrayA.length <= 2) {
-    return middleItem;
+    return [];
   } else if (arrayA.length % 2 === 0) {
-    middleItem.push(Math.ceil(arrayA.length / 2));
-    middleItem.push(Math.ceil(arrayA.length / 2) + 1);
+    return [arrayA[Math.ceil(arrayA.length / 2) - 1], arrayA[Math.ceil(arrayA.length / 2)]];
   } else {
-    middleItem.push(Math.ceil(arrayA.length / 2));
+    return [arrayA[Math.floor(arrayA.length / 2)]];
   }
-  return middleItem;
 };
-
-//test code
-// console.log(middle([1, 2, 3])); // => [2]
-// console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-// console.log(middle([1, 2, 3, 4])); // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
 module.exports = middle;
