2b02c301e7d91438b51c77f518889292f1403745
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-04-13 11:11:10 -0400

    updated to use module in function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,10 +1,9 @@
-//uncomment below to test code
-//const eqObjects = require('./eqObjects');
+const eqObjects = require('./eqObjects');
 
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
-  if (actual === expected) {
+  if (eqObjects(actual, expected) === true) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
@@ -12,8 +11,8 @@ const assertObjectsEqual = function(actual, expected) {
 
 };
 //test code
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertObjectsEqual(eqObjects(ab, ba), true); // => should be true
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba); // => should be true
 
 module.exports = assertObjectsEqual;
